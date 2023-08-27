9db7bbd0197b529139be788dfecd13a335d8a9f6
Gonzalo <gonzasil01@gmail.com>
2023-08-27 18:06:53 -0300

    Arreglo de base de datos

---
+++ CapaPresentacion/ConexionBDD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace CapaPresentacion
+{
+    internal class ConexionBDD
+    {
+            NpgsqlConnection conn = new NpgsqlConnection("Server = silly.db.elephantsql.com; User Id= vuxgyuvc; Password= 9wI083U3JMWjXQ_cdYGTedhOjpdvbXSM; Database= vuxgyuvc; Port= 5432");
+            public void Conectar()
+            {
+                conn.Open();
+                MessageBox.Show("CONECTADO");
+            }
+    }
+}
